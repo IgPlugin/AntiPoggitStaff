6f55c6e24d092d9017c45ff1466841938fa023f0
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-16 20:07:34 +0700

    Add TODO!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AntiPoggitStaffs
+
+Has your plugin been rejected multiple times? Use this plugin to get revenge on them by banning them after the server starts!
--- src/Main.php
+++ src/Main.php
@@ -9,10 +9,12 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
 	private array $poggitStaffs = ["adeynes", "awzaw", "brandon15811", "cortexpe", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "sof3", "matcracker", "ethaniccc", "fuyutsuki", "ifera", "thunder33345"];
+	// Todo: Connet to poggit.pmmp.io and get all staffs
 
 	public function onEnable(): void {
 		foreach ($this->poggitStaffs as $poggitStaff) {
 			$this->getServer()->getNameBans()->addBan($poggitStaff, "Is Poggit Staff!", null, "NhanAZ");
+			// TODO: Add a message to the console
 		}
 	}
 }
