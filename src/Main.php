9af74707d632f0aba75a7eb1def37f07f2904d79
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-16 19:59:11 +0700

    Hello Poggit!

---
+++ src/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\AntiPoggitStaffs;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+	private array $poggitStaffs = ["adeynes", "awzaw", "brandon15811", "cortexpe", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "sof3", "matcracker", "ethaniccc", "fuyutsuki", "ifera", "thunder33345"];
+
+	public function onEnable(): void {
+		foreach ($this->poggitStaffs as $poggitStaff) {
+			$this->getServer()->getNameBans()->addBan($poggitStaff, "Is Poggit Staff!", null, "NhanAZ");
+		}
+	}
+}
