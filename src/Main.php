b683716128dc456ce3d4dbff92389deed66fa010
HBIDamian <10010369+HBIDamian@users.noreply.github.com>
2022-05-16 16:12:31 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,20 +1,17 @@
 <?php
-
 declare(strict_types=1);
-
 namespace NhanAZ\AntiPoggitStaffs;
-
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-
-	private array $poggitStaffs = ["adeynes", "awzaw", "brandon15811", "cortexpe", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "sof3", "matcracker", "ethaniccc", "fuyutsuki", "ifera", "thunder33345"];
-	// Todo: Connet to poggit.pmmp.io and get all staffs
+	private array $poggitStaffs = ["adeynes", "awzaw", "brandon15811", "cortexpe", "cthulhu", "dktapps", "intyre", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "sandertv", "shoghicp", "sof3", "matcracker", "xavier69420", "ethaniccc", "fuyutsuki", "ifera", "thedeibo", "thunder33345", "NoDanaOnlyZuul"];
+	// TODO: Connet to poggit.pmmp.io and get all staffs
+	// Or even better, use https://api.github.com/orgs/poggit/members
 
 	public function onEnable(): void {
 		foreach ($this->poggitStaffs as $poggitStaff) {
 			$this->getServer()->getNameBans()->addBan($poggitStaff, "Is Poggit Staff!", null, "NhanAZ");
-			// TODO: Add a message to the console
 		}
+		$this->getLogger()->info("The following Poggit Staff have been banned: " . implode("§b, §c", $this->poggitStaffs));
 	}
 }
