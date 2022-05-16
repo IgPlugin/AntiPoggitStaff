f8b03ae5354840f098e8a867f0c373660f1aae21
HBIDamian <10010369+HBIDamian@users.noreply.github.com>
2022-05-16 17:52:45 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -4,7 +4,7 @@ namespace NhanAZ\AntiPoggitStaffs;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-	private array $poggitStaffs = ["adeynes", "awzaw", "Botty McBotface", "brandon15811", "brandon15812", "brandon15813", "Cakey Bot", "cortexpe", "cthulhu", "dktapps", "ElonMusk", "ethaniccc", "fuyutsuki", "ifera", "intyre", "IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "MagicalHourglass" "matcracker", "MrsCakeSlayer", "MrsPoggit", "NhanAZ", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98", "robske110", "sandertv", "shoghicp", "sof3", "Spike", "thedeibo", "thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi"];
+	private array $poggitStaffs = ["adeynes", "awzaw", "Botty McBotface", "brandon15811", "brandon15812", "brandon15813", "Cakey Bot", "cortexpe", "cthulhu", "dktapps", "ElonMusk", "ethaniccc", "fuyutsuki", "ifera", "intyre", "IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "MagicalHourglass", "matcracker", "MrsCakeSlayer", "MrsPoggit", "NhanAZ", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98", "robske110", "sandertv", "shoghicp", "sof3", "Spike", "thedeibo", "thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi"];
 	// TODO: Connet to poggit.pmmp.io and get all staffs
 	// Or even better, use https://api.github.com/orgs/poggit/members
 
