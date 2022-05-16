5be8876876db6913035c1f586e6e314c10e86285
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-17 00:39:17 +0700

    Eliminate weeds at the root!

--- src/Main.php
+++ src/Main.php
@@ -4,7 +4,7 @@ namespace NhanAZ\AntiPoggitStaffs;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-	private array $poggitStaffs = ["adeynes", "awzaw", "Botty McBotface", "brandon15811", "brandon15812", "brandon15813", "Cakey Bot", "cortexpe", "coEthaniccc", "cthulhu", "dktapps", "egirlonvalorant", "ElonMusk", "Epicthic", "ethaniccc", "fuyutsuki", "ifera", "intyre", "IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "MagicalHourglass", "matcracker", "mctestDylan", "MrsCakeSlayer", "MrsPoggit", "NhanAZ", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98", "robske110", "sandertv", "shogchips", "shoghicp", "sof3", "SpaceLostC9909", "Spike", "thedeibo", "thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi"];
+	private array $poggitStaffs = ["adeynes", "awzaw", "Botty McBotface", "BobBorrington21", "brandon15811", "brandon15812", "brandon15813", "Cakey Bot", "cortexpe", "coEthaniccc", "cthulhu", "dktapps", "egirlonvalorant", "ElonMusk", "Epicthic", "ethaniccc", "fuyutsuki", "ifera", "intyre", "IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "Jackthehack21", "MagicalHourglass", "matcracker", "mctestDylan", "MrsCakeSlayer", "MrsPoggit", "NhanAZ", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98", "robske110", "sandertv", "shogchips", "shoghicp", "sof3", "SpaceLostC9909", "Spike", "thedeibo", "thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi"];
 	// TODO: Connet to poggit.pmmp.io and get all staffs
 	// Or even better, use https://api.github.com/orgs/poggit/members
 
