21c9b42e90a73287340fc14527fe0df96fcbb818
YTBJero <nguyenhieucoder47@gmail.com>
2022-05-17 06:04:43 +0700

    Members Poggit Github

--- src/Main.php
+++ src/Main.php
@@ -2,12 +2,21 @@
 declare(strict_types=1);
 namespace NhanAZ\AntiPoggitStaffs;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Internet;
 
 class Main extends PluginBase {
 	private array $conflictingPlugins = ["AllPlayersArePoggitStaff"];
-	private array $poggitStaffs = ["adeynes", "awzaw", "Botty McBotface", "BobBorrington21", "brandon15811", "brandon15812", "brandon15813", "Cakey Bot", "cortexpe", "coEthaniccc", "cthulhu", "dktapps", "egirlonvalorant", "ElonMusk", "Epicthic", "ethaniccc", "fuyutsuki", "ifera", "intyre", "IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "Jackthehack21", "MagicalHourglass", "matcracker", "mctestDylan", "MrsCakeSlayer", "MrsPoggit", "NhanAZ", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98", "robske110", "sandertv", "SenpaiJason", "SenpaiJason2.0", "shogchips", "shoghicp", "sof3", "SpaceLostC9909", "Spike", "thedeibo", "thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi"];
+	private array $poggitStaffs = [];
 	// TODO: Connet to poggit.pmmp.io and get all staffs
 	// Or even better, use https://api.github.com/orgs/poggit/members
+
+	public function onLoad(): void {
+		$json = Internet::getURL("https://api.github.com/orgs/poggit/members", 10, [], $err);
+		$json = json_decode($json->getBody(), true);
+		foreach ($json as $data){
+			$this->poggitStaffs[] = $data["login"];
+		}
+	}
 
 	public function onEnable(): void {
 		foreach ($this->poggitStaffs as $poggitStaff) {
