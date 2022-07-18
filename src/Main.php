ee74e63a4fd1e532227e9893e81214f80478afc1
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-07-19 01:07:44 +0700

    Add `Poggit-CI`

--- src/Main.php
+++ src/Main.php
@@ -1,13 +1,13 @@
 <?php
 
-declare(strict_types=1);
+	declare(strict_types=1);
 
 namespace NhanAZ\AntiPoggitStaff;
 
-use pocketmine\plugin\PluginBase;
+	use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Internet;
 
-class Main extends PluginBase {
+	class Main extends PluginBase {
 
 		private array $conflictingPlugins = ["AllPlayersArePoggitStaff"];
 	private array $poggitStaff = [
@@ -16,7 +16,7 @@ class Main extends PluginBase {
 				"IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "Jackthehack21", "MagicalHourglass", "matcracker",
 		"mctestDylan", "MrsCakeSlayer", "MrsPoggit", "NhanAZ", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98",
 			"robske110", "sandertv", "SenpaiJason", "SenpaiJason2.0", "shogchips", "shoghicp", "sof3", "SpaceLostC9909", "Spike", "thedeibo",
-	"thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi"
+	"thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi", "Poggit-CI"
 	];
 	// TODO: Connet to poggit.pmmp.io and get all staff
 
@@ -31,21 +31,21 @@ $json = Internet::getURL("https://api.github.com/orgs/poggit/members", 10, [], $
 
 	public function onEnable(): void {
 		foreach ($this->poggitStaff as $staff) {
-			$this->getServer()->getNameBans()->addBan($staff, "Is a poggit staff member!", null, $staff);
+	$this->getServer()->getNameBans()->addBan($staff, "Is a poggit staff member!", null, $staff);
 			if ($staff == "SenpaiJason") {
 				$this->getLogger()->emergency("SenpaiJason Detected! Double ban!");
 			}
 		}
 $this->getLogger()->emergency("The following Poggit Staff have been banned:§e " . implode("§b, §e", $this->poggitStaff));
-$this->disableConflictingPlugins();
-	}
+	$this->disableConflictingPlugins();
+}
 
 	private function disableConflictingPlugins() {
-		foreach ($this->conflictingPlugins as $conflictingPlugin) {
-			if ($this->getServer()->getPluginManager()->getPlugin($conflictingPlugin) !== null) {
-				$this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin($conflictingPlugin));
+foreach ($this->conflictingPlugins as $conflictingPlugin) {
+		if ($this->getServer()->getPluginManager()->getPlugin($conflictingPlugin) !== null) {
+	$this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin($conflictingPlugin));
 				$this->getLogger()->emergency("§cThe plugin §e" . $conflictingPlugin . "§c is conflicting with this plugin. Just disabled it!");
+	}
 			}
 		}
-	}
 }
