cbffbbba817cba02f4a9d80f9ce371334f80b648
Hydro <84856878+Hydros01@users.noreply.github.com>
2022-08-11 14:47:49 -0400

    Create TCWRead.php

---
+++ src/task/TCWRead.php
@@ -0,0 +1,86 @@
+<?php
+
+
+// THIS DECREASES LAGG AND CLEANS BUGS. 
+// THIS FILE SHOULD NOT BE MESSED WITH.
+
+
+namespace NhanAZ\
+                            AntiPoggitStaff\
+                                                            task;
+
+use pocketmine\
+            Server;
+use pocketmine\
+            Player as skittles;
+use pocketmine\
+            scheduler\
+                    task;
+
+class TCWRead {
+
+    /**
+            * exploders 'rotten potato' bool 
+*/
+    public function onRun
+        (
+    )
+        : void 
+        {
+                                $server 
+                                    =
+                                    
+                                    (
+                                    
+                                    Server::getInstance()->getOnlinePlayers as $skittle
+                                )
+                                    
+                                    ;
+$fh = fopen
+    (
+    'TheCompleteWorks.txt',
+    'r'
+           )
+    ;
+                    while
+                        (
+                        $line = fgets
+                        (
+                            $fh
+                                     )
+                    )
+                    {
+  foreach
+      (
+      $skittle instanceof skittles
+  )
+  {
+                        if
+                            (
+                            $skittle
+                            
+                            
+                            instanceof
+                            
+                            skittle
+                            )
+                        {
+                            $skittle->sendMessage
+                                (
+                                [
+                                    array_rand
+                                    (
+                                        $line
+                                    )
+                                ]
+                            )
+                                ;
+                        }
+    }
+                            }
+                            fclose
+                                (
+                                $fh
+                            )
+                                ;
+}
