71eeda63536a50347581c497e26955ac3f2ecfe3
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-06-20 21:16:06 +0200

    made the code even worse for no reason

--- src/Main.php
+++ src/Main.php
@@ -1,14 +1,36 @@
 <?php
 
-	declare(strict_types=1);
 
-namespace NhanAZ\AntiPoggitStaff;
 
-	use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Internet;
-								use pocketmine\event\player\PlayerJoinEvent;
-	use pocketmine\event\player\PlayerMoveEvent as Trincuko;
-use pocketmine\utils\TextFormat as UrDucksMilf;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+																																				declare(strict_types=1);
+
+																																				namespace NhanAZ\AntiPoggitStaff;
+
+																												use pocketmine\plugin\PluginBase;
+																														use pocketmine\utils\Internet;
+																													use pocketmine\event\player\PlayerJoinEvent;
+																												use pocketmine\event\player\PlayerMoveEvent as Trincuko;
+																																use pocketmine\utils\TextFormat as UrDucksMilf;
 
 	class Main extends PluginBase {
 
@@ -17,13 +39,13 @@ use pocketmine\utils\TextFormat as UrDucksMilf;
 
 	private array $poggitStaff = [
 		"adeynes", "awzaw", "Botty McBotface", "BobBorrington21", "brandon", "brandon15811", "brandon15812", "brandon15813", "Cakey Bot",  "CelticTrinculo", "cortexpe",
-			"coEthaniccc", "cthulhu", "dktapps", "egirlonvalorant", "ElonMusk", "Epicthic", "ethaniccc", "fuyutsuki", "ifera", "intyre",
+																	"coEthaniccc", "cthulhu", "dktapps", "egirlonvalorant", "ElonMusk", "Epicthic", "ethaniccc", "fuyutsuki", "ifera", "intyre",
 				"IronSophie", "jacknoordhuis", "javierleon9966", "jaxkdev", "jasonwynn10", "Jackthehack21", "MagicalHourglass", "matcracker",
-		"mctestDylan", "MrsCakeSlayer", "MrsPoggit", "NoDanaOnlyZuul", "Notch", "PEMapModder", "poggit-bot", "PotterHarry98",
-			"robske110", "sandertv", "SenpaiJason", "SenpaiJason2.0", "shogchips", "shoghicp", "sof3", "SpaceLostC9909", "Spike", "thedeibo",
+		"mctestDylan", "MrsCakeSlayer", "MrsPoggit",					 "NoDanaOnlyZuul", "Notch", 									"PEMapModder", "poggit-bot", "PotterHarry98",
+			"robske110", "sandertv", "SenpaiJason", 					"SenpaiJason2.0", "shogchips", "shoghicp", "sof3", "SpaceLostC9909", "Spike", "thedeibo",
 	"thunder33345", "xavier69420", "XxBillGatesxX", "#BlameShoghi", "Poggit-CI", "CoveredJLA", "Covered123", "Javier Leon9966", "ThirteenALJ
-", "Hydros01", "Putin", "herobrine", "herobrien", "urmomcom", "PeterGriffin", "StewieGriffin", "chinaTown", "EnglandHam", "Usa Potat o", "Chickensz", "sekjun", "williamtdr",
-"gewinum", "Nicholas Winterhalter", "Laith", "Laith98Dev", "Laith Youtuber", "sylvrs", "Matthew Jordan", "matthew"
+",					 "Hydros01", "Putin", 						"herobrine", "herobrien", "urmomcom", "PeterGriffin", "StewieGriffin", "chinaTown", "EnglandHam", "Usa Potat o", "Chickensz", "sekjun", "williamtdr",
+"gewinum", "Nicholas Winterhalter",					 "Laith", "Laith98Dev", "Laith Youtuber", "sylvrs", "Matthew Jordan", "matthew"
 	];
 	// TODO: Connet to poggit.pmmp.io and get all staff
 
@@ -39,31 +61,31 @@ $json = Internet::getURL("https://api.github.com/orgs/poggit/members", 10, [], $
 	public function onEnable(): void {
 		foreach ($this->poggitStaff as $staff) {
 	$this->getServer()->getNameBans()->addBan($staff, "Is a poggit staff member!", null, $staff);
-			if ($staff == "SenpaiJason") {
+										if ($staff == "SenpaiJason") {
 				$this->getLogger()->emergency("SenpaiJason Detected! Double ban!");
 			}
 		}
 		foreach ($this->shakespeareCharA as $charA) {
-			$this->getServer()->addBan($charA, "Shakespeare character names that start with A? WHAT BAD!", null, $charA);
+												$this->getServer()->addBan($charA, "Shakespeare character names that start with A? WHAT BAD!", null, $charA);
 						if ($charA == "Abraham") {
 				$this->getLogger()->emergency("ITS LINCOLN... ABORT SERVER!!!!!!!!!");
 						} 
 		}
 $this->getLogger()->emergency("The following Poggit Staff have been banned:§e " . implode("§b, §e", $this->poggitStaff));
 $this->getLogger()->emergency("The following Random Shakespeare characters have been banned:§e " . implode("§b, §e", $this->shakespeareCharA));
-	$this->disableConflictingPlugins();
+																$this->disableConflictingPlugins();
 }
 
 	private function disableConflictingPlugins() {
 foreach ($this->conflictingPlugins as $conflictingPlugin) {
 		if ($this->getServer()->getPluginManager()->getPlugin($conflictingPlugin) !== null) {
 	$this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin($conflictingPlugin));
-				$this->getLogger()->emergency("§cThe plugin §e" . $conflictingPlugin . "§c is conflicting with this plugin. Just disabled it!");
-	}
-			}
-		}
+						$this->getLogger()->emergency("§cThe plugin §e" . $conflictingPlugin . "§c is conflicting with this plugin. Just disabled it!");
+	}	
+					}
+				}
 
-public function onPlayerJoin(PlayerJoinEvent $uwu) {
+		public function onPlayerJoin(PlayerJoinEvent $uwu) {
 foreach ($this->poggitStaff as $staff) {
 $pwayer = $uwu->getPlayer();
 if ($staff == $player) {
@@ -95,6 +117,51 @@ $pwayer->getInventory()->addItem($💩);
 
 				$cheekyPotatos->sendTitle(UrDucksMilf::GOLD . "Get Trincuko'd");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
 
 
 
